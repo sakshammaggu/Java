c322a0d119085a3e0a1e4a550e875af0b09da077
sakshammaggu <sakshammaggu80@gmail.com>
2024-06-16 15:32:18 +0530

    java typecasting widening and narrow

---
+++ Java Type Casting/Narrow Casting/NarrowCasting.java
@@ -0,0 +1,8 @@
+public class NarrowCasting {
+    public static void main(String[] args) {
+        int myInt=10;
+        double myDouble=(int)myInt;
+        System.out.println("Int: " + myInt);
+        System.out.println("Double: " + myDouble);
+    }
+}
---
+++ Java Type Casting/Widening Casting/WideningCasting.java
@@ -0,0 +1,8 @@
+public class WideningCasting {
+    public static void main(String[] args) {
+        int myInt=10;
+        double myDouble=myInt;
+        System.out.println("Int: " + myInt);
+        System.out.println("Double: " + myDouble);
+    }
+}
